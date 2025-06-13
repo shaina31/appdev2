7860faae73034484f766322d1bacd5ae7be791a8
shaina31 <80665132+shaina31@users.noreply.github.com>
2025-06-14 03:05:38 +0800

    Delete 11_simple_book_api_with_email_notification directory

--- 11_simple_book_api_with_email_notification/routers/AuthRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const AuthController = require('../controllers/AuthController');
-
-router.post('/signup', AuthController.signup);
-router.post('/signin', AuthController.signin);
-
-module.exports = router;
